eb171bc9680bceced4bb507cd505374b0297f904
jhon garcia <jhon19.2@outlook.com>
2020-06-29 01:56:51 -0400

    Initial commit

---
+++ piedra_papel _y_tijera/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace piedra_papel__y_tijera
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int puntosMaquina = 0;
+            int puntosHumano = 0;
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Jugar piedra, papel o tijera maquina vs hombre");
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ingrese 0 para piedra");
+                Console.WriteLine("Ingrese 1 para papel");
+                Console.WriteLine("Ingrese 2 para tijera");
+                Console.Write(">"); // puro diseño
+                int humano = int.Parse(Console.ReadLine());
+                int maquina = rnd.Next(0, 3);
+
+                if (humano == 0 && maquina == 2)
+                {
+                    puntosHumano++;
+                }
+                else
+                {
+                    if (humano == 2 && maquina == 0)
+                    {
+                        puntosMaquina++;
+                    }
+                }
+
+                if (humano == 1 && maquina == 0)
+                {
+                    puntosHumano++;
+                }
+                else
+                {
+                    if (humano == 0 && maquina == 1)
+                    {
+                        puntosMaquina++;
+                    }
+                }
+
+                if (humano == 2 && maquina == 1)
+                {
+                    puntosHumano++;
+                }
+                else
+                {
+                    if (humano == 1 && maquina == 2)
+                    {
+                        puntosMaquina++;
+                    }
+                }
+
+
+
+            }
+
+            if (puntosHumano == puntosMaquina)
+            {
+                Console.WriteLine("Empate");
+            }
+            else if (puntosHumano > puntosMaquina)
+            {
+                Console.WriteLine("Gano Humano");
+            }
+            else
+            {
+                Console.WriteLine("Gano Maquina");
+            }
+            
+        }
+    }
+}
---
+++ piedra_papel _y_tijera/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("piedra_papel__y_tijera")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("piedra_papel__y_tijera")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D6A4B629-4008-42C3-B299-ECB4D0B1B43F")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
